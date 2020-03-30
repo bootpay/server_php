f9cb5f37fa9a023b89f8057b9983337ba064e76a
Gosomi <gosomi@forelf.com>
2020-03-30 18:25:31 +0900

    예제 추가

--- test/GetUserToken.php
+++ test/GetUserToken.php
@@ -13,12 +13,15 @@ $bootpay = BootpayApi::setConfig(
 );
 
 $response = $bootpay->requestAccessToken();
-var_dump($response);
 
 if ($response->status === 200) {
     $result = $bootpay->getUserToken([
         'user_id' => '[[ 회원 아이디 ]]', # 필수
-
+        'email' => '[[ 이메일 ]]', # 선택
+        'name' => '[[ 회원 이름 ]]', # 선택
+        'gender' => '[[ 회원 성별, 0 - 여자, 1 - 남자 ]]', # 선택
+        'birth' => '[[ 회원 생년월일 6자리 ]]', # 선택
+        'phone' => '[[ 회원의 연락가능한 전화번호 ]]' # 페이앱의 경우만 필수, 나머지 선택
     ]);
     var_dump($result);
 }
